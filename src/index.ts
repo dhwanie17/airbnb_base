a5e3cea1138ef4490c551e9bff12ebef0f97fde5
dhwanie17 <dhwani.shah@sarvadhi.com>
2022-07-14 15:20:49 +0530

    Modified files with detailed rules

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,47 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
 const port = process.env.PORT;
 
 //unused variable
 const x = 10;
+var x,
+    x = 10;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+function doSomething() {
+    var foo = 2;
+}
+
+const object2 = {
+    prope: 12,
+};
+console.log(object2);
+
+const foo = () => {
+    console.log("Function Running");
+};
+foo();
+
+var foo = {
+    bar: "This is a bar.",
+    baz: { qux: "This is a qux" },
+    difficult: "to read",
+};
+
+function bar() {
+    // ...
+}
+bar();
+
+const string = "Hiiiii";
+
+const string2 = '"Hiii" How are you?';
+console.log(string);
+
+function* generator() {}
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server");
 });
 
 app.listen(port, () => {
